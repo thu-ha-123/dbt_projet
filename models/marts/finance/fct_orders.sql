9b91d8f1aebeaf1cfb03d4f9ca575bc51f17d26f
Thu Ha TRAN <TRANTHUHA.134@gmail.com>
2025-12-25 08:42:45 +0000

    created fact order table, created folder marketing under marts, added jaffle_shop yaml file.

---
+++ models/marts/finance/fct_orders.sql
@@ -0,0 +1,27 @@
+with orders as (
+    select * from {{ ref('stg_jaffle_shop__orders') }}), 
+
+payments as (
+    select * from {{ ref('stg_stripe__payments') }}
+),
+order_payment as (
+    select
+        order_id,
+        sum(case 
+                when status = 'success' then amount
+                else null 
+                end) as amount
+    from payments
+    group by 1
+),
+final as (
+    select
+        orders.order_id,
+        orders.customer_id,
+        orders.order_date,
+        coalesce(order_payment.amount, 0) as amount
+    from orders
+    left join order_payment using (order_id)
+)
+
+select * from final
