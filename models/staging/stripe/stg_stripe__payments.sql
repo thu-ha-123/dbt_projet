c9d421a8177a8da82491d1dc1d7c607e58604561
Thu Ha TRAN <TRANTHUHA.134@gmail.com>
2025-12-26 16:50:45 +0000

    added singular test to stg_stripe__payment

--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -2,7 +2,7 @@ select
     id as payment_id,
     orderid as order_id,
     paymentmethod as payment_method,
-    status,
+    status as payment_status,
     amount / 100 as amount,
     created as created_at
-from {{ source('stripe', 'payments') }}
+from {{ source('stripe', 'payment') }}
