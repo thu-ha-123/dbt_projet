12fb5e6387f685401fb478c7c89d009f583d8640
Thu Ha TRAN <tranthuha.134@gmail.com>
2025-12-28 21:01:15 +0100

    updated fct_orders model (#3)

--- models/marts/finance/fct_orders.sql
+++ models/marts/finance/fct_orders.sql
@@ -4,7 +4,7 @@ with orders as (
 payments as (
     select * from {{ ref('stg_stripe__payments') }}
 ),
-order_payment as (
+order_payments as (
     select
         order_id,
         sum(case 
@@ -19,9 +19,9 @@ final as (
         orders.order_id,
         orders.customer_id,
         orders.order_date,
-        coalesce(order_payment.amount, 0) as amount
+        coalesce (order_payments.amount, 0) as amount
     from orders
-    left join order_payment using (order_id)
+    left join order_payments using (order_id)
 )
 
 select * from final
