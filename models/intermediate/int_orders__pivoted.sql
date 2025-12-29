349b1299962e411f03a5adddeebeed05f5cf9e82
Thu Ha TRAN <TRANTHUHA.134@gmail.com>
2025-12-29 16:02:48 +0000

    added intermediate model and macro

---
+++ models/intermediate/int_orders__pivoted.sql
@@ -0,0 +1,24 @@
+with payments as (
+
+    select *
+    from {{ ref('stg_stripe__payments') }}
+    where status ='success'
+)
+, pivoted as (
+    select order_id,
+            {%- set payment_methods=['bank_transfer', 'coupon', 'credit_card', 'gift_card', 'bitcoin'] -%}
+
+            {% for payment_method in payment_methods %}
+                sum(case when payment_method = '{{ payment_method}}' then amount else 0 end) as {{ payment_method }}_amount
+                {%- if not loop.last -%}
+                    ,
+                {%- endif -%}
+                
+            {%- endfor -%}
+    
+    
+    from payments
+    group by 1
+)
+select *
+from pivoted
--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -3,6 +3,6 @@ select
     orderid as order_id,
     paymentmethod as payment_method,
     status as payment_status,
-    amount / 100 as amount,
+    {{ cents_to_dollars("amount",4) }} as amount,
     created as created_at
 from {{ source('stripe', 'payment') }}
