4745059e1307bc51be2972c83683b72d19d9d1ab
Thu Ha TRAN <TRANTHUHA.134@gmail.com>
2026-01-03 07:16:23 +0000

    added audit_helper package, int model for orders and fct_customer_orders

--- models/marts/fct_customer_orders.sql
+++ models/marts/fct_customer_orders.sql
@@ -1,36 +1,13 @@
 with
-    -- import CTEs
-    customers as (select * from {{ ref('stg_jaffle_shop__customers') }}),
-    orders as (select * from {{ ref('stg_jaffle_shop__orders') }}),
-    payments as (select * from {{ ref('stg_stripe__payments') }}),
-
--- logical CTEs
-    completed_payments as (
-        select
-            order_id,
-            max(payment_created_at) as payment_finalized_date,
-            sum(payment_amount) as total_amount_paid
-        from payments
-        where payment_status != 'fail'
-        group by 1
+    customers as (select * from {{ ref('stg_jaffle_shop__customers') }}
     ),
 
     paid_orders as (
         select
-            orders.order_id,
-            orders.customer_id,
-            orders.order_placed_at,
-            orders.order_status,
-            completed_payments.total_amount_paid,
-            completed_payments.payment_finalized_date,
-            customers.customer_first_name,
-            customers.customer_last_name
-        from orders
-        left join completed_payments on orders.order_id = completed_payments.order_id
-        left join customers on orders.customer_id = customers.customer_id
+            *
+        from {{ ref('int_orders') }}
     ),
     
-    -- Final CTE
     final as (
         select
             
