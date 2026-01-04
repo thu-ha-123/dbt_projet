4ebff47c3c4d50bdc38be741efbad72e6fe0db97
Thu Ha TRAN <tranthuha.134@gmail.com>
2026-01-04 11:48:42 +0100

    Dbt advanced/refactoring sql for modularity (#5)
    * Added customer_orders_legacy and fct_customer_orders
    
    * updated stg customers, orders, payments and fct customer orders
    
    * added audit_helper package, int model for orders and fct_customer_orders
    
    * added incremental model to fct_orders

---
+++ models/marts/fct_orders.sql
@@ -0,0 +1,41 @@
+
+{{
+    config(
+        materialized='incremental',
+        unique_key = 'order_id',
+        incremental_strategy = 'merge',
+    )
+}}
+
+with orders as (
+    select * from {{ ref('stg_jaffle_shop__orders') }}
+), 
+customers as (
+    select * from {{ ref('stg_jaffle_shop__customers') }}
+),
+payments as (
+    select * from {{ ref('stg_stripe__payments') }}
+),
+order_payments as (
+    select 
+        order_id,
+        sum(case when payment_status = 'success' then payment_amount end) as amount
+    from payments
+    group by 1
+), 
+final as (
+    select
+        orders.order_id,
+        orders.customer_id,
+        orders.order_placed_at,
+        coalesce(order_payments.amount, 0) as amount
+    from orders 
+    left join order_payments using (order_id)
+)
+select * from final 
+
+{% if is_incremental() %}
+where
+order_placed_at >= (select max(order_placed_at) from {{this}})
+{% endif %}
+
