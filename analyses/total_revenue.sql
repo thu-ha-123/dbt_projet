5cfe747f3e7e4a46caa53282fa3a8ff83fabb70c
Thu Ha TRAN <TRANTHUHA.134@gmail.com>
2026-01-05 14:17:49 +0000

    updated 'total_revenue' analyses file

--- analyses/total_revenue.sql
+++ analyses/total_revenue.sql
@@ -1,6 +1,15 @@
-select 
-    sum(
-        case
-        when payment_status = 'success' then payment_amount
-        else null) as total_revenue
-from {{ ref('stg_stripe__payments') }}
+with payments as (
+    select *
+    from {{ ref('stg_stripe__payments') }}
+),
+aggregated as (
+    select
+        sum(
+            case
+            when payment_status = 'success' then payment_amount
+            else null 
+            end) as total_revenue 
+    from payments
+)
+
+select * from aggregated
