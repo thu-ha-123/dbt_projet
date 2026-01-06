aa64523713f7406c7d6b2d1edbec068254072a95
Thu Ha TRAN <TRANTHUHA.134@gmail.com>
2026-01-06 09:33:32 +0000

    added model version 2 of fct_orders model

---
+++ models/marts/fct_orders_v2.sql
@@ -0,0 +1,42 @@
+
+{{
+    config(
+        materialized='incremental',
+        on_schema_change='fail',
+        unique_key = 'order_id',
+        incremental_strategy = 'merge',
+    )
+}}
+
+with orders as (
+    select * from {{ ref('stg_jaffle_shop__orders') }}
+), 
+customers as (
+    select * from {{ ref('stg_jaffle_shop__customers') }}
+),
+payments as (
+    select * from {{ ref('stg_stripe__payments') }}
+),
+order_payments as (
+    select 
+        order_id,
+        sum(case when payment_status = 'success' then payment_amount end) as amount
+    from payments
+    group by 1
+), 
+final as (
+    select
+        cast(orders.order_id as text) as order_id,
+        cast(orders.customer_id as text) as customer_id ,
+        orders.order_placed_at,
+        coalesce(order_payments.amount, 0) as total_amount
+    from orders 
+    left join order_payments using (order_id)
+)
+select * from final 
+
+{% if is_incremental() %}
+where
+order_placed_at >= (select max(order_placed_at) from {{this}})
+{% endif %}
+
