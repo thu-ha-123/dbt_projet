fa381df4d19d86a0affeb37fbee85f8c33fd9aea
Thu Ha TRAN <tranthuha.134@gmail.com>
2026-01-07 17:05:06 +0100

    Advanced testing (#11)
    * added dbt_utils and dbt_expectation packages
    
    * added singulars and generic tests
    
    * added cardinality_equality test to fct_orders model
    
    * added test configuration for _src_stripe__payments

---
+++ tests/generic/assert_dollars_spent_are_greater_than_one.sql
@@ -0,0 +1,10 @@
+{% test assert_dollars_spent_are_greater_than_one(model, column_name, group_by_column) %}
+
+    select
+        {{ group_by_column }}, 
+        avg( {{ column_name }}) as average_amount
+    from {{ model }}
+    group by {{ group_by_column }}
+    having avg( {{ column_name }}) < 1
+
+{% endtest %}
