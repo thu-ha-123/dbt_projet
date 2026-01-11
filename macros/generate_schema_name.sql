746ac9adaa1f3a202ce395cfb3667c080bf5ebe9
Thu Ha TRAN <TRANTHUHA.134@gmail.com>
2026-01-11 09:53:57 +0000

    added macro generate_schema_name and updated test for payment source

---
+++ macros/generate_schema_name.sql
@@ -0,0 +1,22 @@
+{% macro generate_schema_name(custom_schema_name, node) -%}
+
+    {%- set default_schema = target.schema -%}
+    {%- if custom_schema_name is none -%}
+
+        {{ default_schema }}
+
+    -- if we are in prod
+        -- then we just want custom_schema_name
+
+    {%- elif env_var('DBT_MY_ENV','') == 'prod' -%}
+
+       {{ custom_schema_name | trim }}
+
+
+    {%- else -%}
+
+        {{ default_schema }}_{{ custom_schema_name | trim }}
+
+    {%- endif -%}
+
+{%- endmacro %}
